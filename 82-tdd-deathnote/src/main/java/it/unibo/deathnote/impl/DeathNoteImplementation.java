910598bd86789e930ddc1ae52fdc9f6cf6ced630
Alessandro Gardini <alessandro.gardini7@studio.unibo.it>
2024-11-17 15:02:18 +0100

    now all method throws exceptions (IllegalArgumentsException)

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -3,7 +3,6 @@ package it.unibo.deathnote.impl;
 import it.unibo.deathnote.api.DeathNote;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -29,42 +28,82 @@ public class DeathNoteImplementation implements DeathNote {
 
     @Override
     public boolean writeDeathCause(String cause) {
+        checkString(cause);
         return deathMap.get(currentDying).setCause(cause);
     }
 
     @Override
     public boolean writeDetails(String details) {
-        return false;
+        checkString(details);
+        return deathMap.get(currentDying).setDetails(details);
     }
 
     @Override
     public String getDeathCause(String name) {
-        return "";
+        checkName(name);
+        return deathMap.get(name).getCause();
     }
 
     @Override
     public String getDeathDetails(String name) {
-        return "";
+        checkName(name);
+        return deathMap.get(name).getDetails();
     }
 
     @Override
     public boolean isNameWritten(String name) {
-        return false;
+        return deathMap.containsKey(name);
+    }
+
+    private void checkString(String string) {
+        if(string == null) {
+            throw new IllegalArgumentException("Name must be not null");
+        }
+        if(currentDying == null) {
+            throw new IllegalArgumentException("A string must be in Set");
+        }
+    }
+
+    private void checkName(String name) {
+        if(name == null) {
+            throw new IllegalArgumentException("Name must be not null");
+        }
+        if(!deathMap.containsKey(name)) {
+            throw new IllegalArgumentException("Name not contained in Set");
+        }
     }
 
     private final static class Death {
         private final static String DEFAULT_CAUSE = "HEART_ATTACK";
-        private String cause = DEFAULT_CAUSE;
+        private final static String DEFAULT_DETAILS = null;
+        public static final int MAX_TIME_WRITE_CAUSE = 40;
+        public static final int MAX_TIME_WRITE_DETAILS = 6040;
+        private String cause;
         private String details;
-        private final long initialTime = System.currentTimeMillis();
+        private final long initialTime;
 
-        public void setDetails(String details) {
-            this.details = details;
+        public Death() {
+            this.cause = DEFAULT_CAUSE;
+            this.details = DEFAULT_DETAILS;
+            initialTime = System.currentTimeMillis();
         }
 
         public boolean setCause(String cause) {
-            if()
-            this.cause = cause;
+            if(System.currentTimeMillis() < (initialTime + MAX_TIME_WRITE_CAUSE)){
+                this.cause = cause;
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public boolean setDetails(String details) {
+            if(System.currentTimeMillis() < (initialTime + MAX_TIME_WRITE_DETAILS)){
+                this.details = details;
+                return true;
+            } else {
+                return false;
+            }
         }
 
         public String getCause() {
